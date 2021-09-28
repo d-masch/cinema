929c0bd205302218e23a675f5e84ff01089bd26d
d-masch <energodesign@yandex.ru>
2021-09-28 22:26:33 +0300

    Т-14 + разработка сайта 28.09.21

--- js/film-table.js
+++ js/film-table.js
@@ -1,36 +1,59 @@
 const films = [
-  {time: "10.00",
-   name: "Человек-паук",
-   genres: [ "Фантастика", "боевик", "приключения" ],
-  },   
-  {time: "12.00",
-   name: "Собачья жизнь 2",
-   genres: [ "фентези", "драмма", "комедия" ],
+  {
+    time: "10.00",
+    name: "Человек-паук",
+    genres: ["Фантастика", "боевик", "приключения"],
   },
-  {time: "14.00",
-   name: "История игрушек 4",
-   genres: [ "Мультфильмы", "фентези", "комедия" ],
+  {
+    time: "12.00",
+    name: "Собачья жизнь 2",
+    genres: ["фентези", "драмма", "комедия"],
   },
-  {time: "16.00",
-   name: "Люди в черном: Интернешнл",
-   genres: [ "Фантастика", "боевик", "комедия" ],
-  }, 
+  {
+    time: "14.00",
+    name: "История игрушек 4",
+    genres: ["Мультфильмы", "фентези", "комедия"],
+  },
+  {
+    time: "16.00",
+    name: "Люди в черном: Интернешнл",
+    genres: ["Фантастика", "боевик", "комедия"],
+  },
+  {
+    time: "18.00",
+    name: "Человек-паук",
+    genres: ["Фантастика", "боевик", "приключения"],
+  },
+  {
+    time: "20.00",
+    name: "Собачья жизнь 2",
+    genres: ["фентези", "драмма", "комедия"],
+  },
+  {
+    time: "22.00",
+    name: "История игрушек 4",
+    genres: ["Мультфильмы", "фентези", "комедия"],
+  },
+  {
+    time: "23.00",
+    name: "Люди в черном: Интернешнл",
+    genres: ["Фантастика", "боевик", "комедия"],
+  },
 ];
-let tbody = document.getElementById('table-body');
-console.log(tbody.innerHTML);
-
-tbody.innerHTML = '
+let tbody = document.getElementById("table-body");
+tbody.innerHTML = "";
+for (const film of films) {
+  tbody.innerHTML += `
 <tr>
       <td> 
-          <div style="margin-left:25px;">
+          <div class="table-body-td">
                <img src="./images/Stroke.png" alt="галка"></img>
           </div>
       </td>
-      <td> 10.00</td>
-      <td><a href="https://www.kinopoisk.ru/film/838/" target="_blank"> Человек паук</a></td>
-      <td>Фантастика, боевик, приключения</td>
-</tr> '
-;
-
-
-
+      <td>${film.time}</td>
+      <td><a href="https://www.kinopoisk.ru/film/838/" target="_blank">${
+        film.name
+      }</a></td>
+      <td>${film.genres.join(", ")}</td>
+</tr> `;
+}
