a244072ffdd3922cfc9f4a81ecf69d2cfbd555a4
d-masch <energodesign@yandex.ru>
2021-11-26 23:26:36 +0300

    homework - 17

--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,91 @@
 const prizeForm = document.getElementById("prize-popup");
 const openBtn = document.querySelector("#popup-open");
 const closeBtn = document.querySelector("#popup-close");
-const nameInputWrapper = document.querySelector("#prize-popup input[name='name']").parentNode;
-const emailInputWrapper = document.querySelector("#prize-popup input[name='e-mail']").parentNode;
+const nameInputWrapper = document.querySelector(
+    "#prize-popup input[name='name']"
+).parentNode;
+const emailInputWrapper = document.querySelector(
+    "#prize-popup input[name='e-mail']"
+).parentNode;
+const prizeSelect = document.querySelector('#choose-prize');
+
+const INPUT_ERROR_CLASS = "st-input1_error";
+const INPUT_FOCUS_CLASS = "st-input1_focused";
 function popupToggle() {
     prizeForm.classList.toggle("hidden");
 }
-// function initializeField(field) {
-//     const input = field.getElementByTagName('input')[0];
-//     const errorText = field.querySelector('st-input1__error-msg');
-//     input.value = '';
-//     errorText.innerText = '';
-// }
+function initializeField(field) {
+    const input = field.getElementsByTagName('input')[0];
+    const errorText = field.querySelector('.st-input1__error-msg');
+    clearError();
+    field.classList.remove(INPUT_FOCUS_CLASS);
+    input.value = '';
 
-// initializeField(nameInputWrapper);
-// initializeField(emailInputWrapper);
-openBtn.onclick = popupToggle;
+    function clearError() {
+        field.classList.remove(INPUT_ERROR_CLASS);
+        errorText.innerText = '';
+    }
+    input.addEventListener('focus', function () {
+        field.classList.add(INPUT_FOCUS_CLASS);
+    });
+    input.addEventListener('input', function () {
+        clearError();
+    });
+    input.addEventListener('blur', function () {
+        if (!input.value) {
+            field.classList.remove(INPUT_FOCUS_CLASS);
+        }
+    });
+
+    return {
+        focus() {
+            input.focus();
+        },
+        getValue() {
+            return input.value;
+        },
+        setError(error) {
+            errorText.innerText = error;
+            field.classList.add(INPUT_ERROR_CLASS);
+        },
+    };
+}
+
+const nameField = initializeField(nameInputWrapper);
+const emailField = initializeField(emailInputWrapper);
+
+openBtn.onclick = function () {
+    popupToggle();
+    nameField.focus();
+};
+
 closeBtn.onclick = popupToggle;
+prizeForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const nameValue = nameField.getValue();
+    const emailValue = emailField.getValue();
+
+    if (!nameValue) {
+        nameField.setError("Поле обязательно для заполнения");
+        nameField.focus();
+        return;
+    }
+    if (!emailValue) {
+        emailField.setError("Поле обязательно для заполнения");
+        nameField.focus();
+        return;
+    }
+    if (prizeSelect.value === 'none'){
+        prizeSelect.classList.add(INPUT_ERROR_CLASS);
+        return;
+    }
+    const data = {
+        name: nameValue,
+        email: emailValue,
+        prize: prizeSelect.value,
+    }
+    const url = new URL('http://inno-ijl.ru/multystub/stc-21/feedback');
+    url.searsh = new URLSearchParams(data).toString();
+    fetch(url.toString());
+});
--- target/popup.js
+++ target/popup.js
@@ -5,18 +5,88 @@ var openBtn = document.querySelector("#popup-open");
 var closeBtn = document.querySelector("#popup-close");
 var nameInputWrapper = document.querySelector("#prize-popup input[name='name']").parentNode;
 var emailInputWrapper = document.querySelector("#prize-popup input[name='e-mail']").parentNode;
+var prizeSelect = document.querySelector('#choose-prize');
+var INPUT_ERROR_CLASS = "st-input1_error";
+var INPUT_FOCUS_CLASS = "st-input1_focused";
 
 function popupToggle() {
   prizeForm.classList.toggle("hidden");
-} // function initializeField(field) {
-//     const input = field.getElementByTagName('input')[0];
-//     const errorText = field.querySelector('st-input1__error-msg');
-//     input.value = '';
-//     errorText.innerText = '';
-// }
-// initializeField(nameInputWrapper);
-// initializeField(emailInputWrapper);
+}
 
+function initializeField(field) {
+  var input = field.getElementsByTagName('input')[0];
+  var errorText = field.querySelector('.st-input1__error-msg');
+  clearError();
+  field.classList.remove(INPUT_FOCUS_CLASS);
+  input.value = '';
 
-openBtn.onclick = popupToggle;
+  function clearError() {
+    field.classList.remove(INPUT_ERROR_CLASS);
+    errorText.innerText = '';
+  }
+
+  input.addEventListener('focus', function () {
+    field.classList.add(INPUT_FOCUS_CLASS);
+  });
+  input.addEventListener('input', function () {
+    clearError();
+  });
+  input.addEventListener('blur', function () {
+    if (!input.value) {
+      field.classList.remove(INPUT_FOCUS_CLASS);
+    }
+  });
+  return {
+    focus: function focus() {
+      input.focus();
+    },
+    getValue: function getValue() {
+      return input.value;
+    },
+    setError: function setError(error) {
+      errorText.innerText = error;
+      field.classList.add(INPUT_ERROR_CLASS);
+    }
+  };
+}
+
+var nameField = initializeField(nameInputWrapper);
+var emailField = initializeField(emailInputWrapper);
+
+openBtn.onclick = function () {
+  popupToggle();
+  nameField.focus();
+};
+
 closeBtn.onclick = popupToggle;
+prizeForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var nameValue = nameField.getValue();
+  var emailValue = emailField.getValue();
+
+  if (!nameValue) {
+    nameField.setError("Поле обязательно для заполнения");
+    nameField.focus();
+    return;
+  }
+
+  if (!emailValue) {
+    emailField.setError("Поле обязательно для заполнения");
+    nameField.focus();
+    return;
+  }
+
+  if (prizeSelect.value === 'none') {
+    prizeSelect.classList.add(INPUT_ERROR_CLASS);
+    return;
+  }
+
+  var data = {
+    name: nameValue,
+    email: emailValue,
+    prize: prizeSelect.value
+  };
+  var url = new URL('http://inno-ijl.ru/multystub/stc-21/feedback');
+  url.searsh = new URLSearchParams(data).toString();
+  fetch(url.toString());
+});
